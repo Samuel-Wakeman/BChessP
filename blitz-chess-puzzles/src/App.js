f1c53060830351613968d4a2fa08c985bbfb49eb
Samuel Wakeman <sam.wakeman@gmail.com>
2021-11-18 12:06:29 -0500

    add and attribute 3 images

--- blitz-chess-puzzles/src/App.js
+++ blitz-chess-puzzles/src/App.js
@@ -18,7 +18,20 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+      <div> knight made by <a href="https://www.freepik.com" 
+                            title="Freepik">Freepik</a> from <a 
+                            href="https://www.flaticon.com/" 
+                            title="Flaticon">www.flaticon.com</a>,
+            pawn made by   <a href="https://www.flaticon.com/authors/pixel-perfect" 
+                            title="Pixel perfect">Pixel perfect</a> from <a 
+                            href="https://www.flaticon.com/" 
+                            title="Flaticon">www.flaticon.com</a>,
+            queen made by  <a href="https://www.flaticon.com/authors/deemakdaksina" 
+                              title="deemakdaksina">deemakdaksina</a> from 
+                           <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+                                           
+      </div>
+    </div>    
   );
 }
 

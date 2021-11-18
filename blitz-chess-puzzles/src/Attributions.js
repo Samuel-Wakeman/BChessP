8be5a071c40c3ec00c936eb1d033be0d04f6276a
Samuel Wakeman <sam.wakeman@gmail.com>
2021-11-18 12:45:03 -0500

    update name and add chessboard

--- blitz-chess-puzzles/src/Attributions.js
+++ blitz-chess-puzzles/src/Attributions.js
@@ -1,36 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import './attributions.css';
 
-function App() {
+function Attributions() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="attributions">      
       <div> Knight made by <a href="https://www.freepik.com" 
                             title="Freepik">Freepik</a> from <a 
                             href="https://www.flaticon.com/" 
-                            title="Flaticon">www.flaticon.com</a>,
+                            title="Flaticon">www.flaticon.com</a>, <br></br>
             Pawn made by   <a href="https://www.flaticon.com/authors/pixel-perfect" 
                             title="Pixel perfect">Pixel perfect</a> from <a 
                             href="https://www.flaticon.com/" 
-                            title="Flaticon">www.flaticon.com</a>,
+                            title="Flaticon">www.flaticon.com</a>, <br></br>
             Rook,
             Queen 
             and King made by  <a href="https://www.flaticon.com/authors/deemakdaksina" 
-                              title="deemakdaksina">deemakdaksina</a> from 
-                           <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+                              title="deemakdaksina">deemakdaksina</a>from  
+                           <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>, <br></br>
             Bishop made by <a href="https://www.flaticon.com/authors/victoruler" 
                               title="Victoruler">Victoruler</a> from <a 
                               href="https://www.flaticon.com/" 
@@ -41,4 +26,4 @@ function App() {
   );
 }
 
-export default App;
+export default Attributions;
--- blitz-chess-puzzles/src/index.js
+++ blitz-chess-puzzles/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Attributions from './Attributions';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Attributions/>
   </React.StrictMode>,
   document.getElementById('root')
 );
